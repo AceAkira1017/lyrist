f0b6ad8113cf1afc8326c32dc98113805ac9bcac
ItZmeAc3 <153344910+AceAkira1017@users.noreply.github.com>
2024-12-27 00:21:21 +0800

    Update Meta.tsx

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -1,35 +1,31 @@
 import Head from "next/head";
 
-export default function Meta() {
+interface MetaProps {
+  title?: string;
+  description?: string;
+  image?: string;
+}
+
+export default function Meta({
+  title = "lyrist",
+  description = "Simple yet powerful RESTful lyrics API",
+  image = "https://user-images.githubusercontent.com/68690233/211316118-3adf4ec2-4082-46c2-979e-7d2127290532.png",
+}: MetaProps) {
   return (
     <Head>
-      <title>lyrist</title>
-      <link rel="icon" href="https://fmj.asrvd.me/🎶"></link>
-      <meta
-        name="description"
-        content="Simple yet powerful RESTful lyrics API"
-      />
+      <title>{title}</title>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <meta name="description" content={description} />
       <meta name="theme-color" content="#fce7f3" />
       <meta property="og:site_name" content="lyrist" />
-      <meta property="og:title" content="lyrist" />
+      <meta property="og:title" content={title} />
       <meta property="og:type" content="website" />
-      <meta
-        property="og:description"
-        content="simple yet powerful RESTful lyrics API"
-      />
-      <meta property="twitter:title" content="lyrist" />
-      <meta
-        property="twitter:description"
-        content="simple yet powerful RESTful lyrics API"
-      />
-      <meta
-        property="og:image"
-        content="https://user-images.githubusercontent.com/68690233/211316118-3adf4ec2-4082-46c2-979e-7d2127290532.png"
-      />
-      <meta
-        property="twitter:image"
-        content="https://user-images.githubusercontent.com/68690233/211316118-3adf4ec2-4082-46c2-979e-7d2127290532.png"
-      />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      <meta property="og:url" content="https://yourwebsite.com" />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={image} />
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:site" content="@itzmeac3_" />
     </Head>
