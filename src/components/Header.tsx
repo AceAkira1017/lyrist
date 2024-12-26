8aa8d634d313ae76fd65ec57bc649a3a4c4ce1f5
ItZmeAc3 <153344910+AceAkira1017@users.noreply.github.com>
2024-12-27 00:24:38 +0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,25 +16,26 @@ export default function Header() {
   const path = router.asPath;
 
   return (
-    <nav className="absolute top-0 w-full p-6 font-sans text-sm text-zinc-300 md:w-2/3 md:p-4 lg:w-1/2 flex gap-2">
+    <p className="absolute py-4 top-0 w-full p-6 font-sans text-sm text-zinc-300 md:w-2/3 md:p-4 lg:w-1/2 flex gap-2">
       {navLinks.map((link, index) => {
-        const isActive = link.path === path;
-
-        return (
-          <a
-            key={index}
-            href={link.path}
-            className={`duration-200 rounded-md p-2 ${
-              isActive
-                ? "font-semibold text-pink-200 bg-zinc-800/60 border border-zinc-600"
-                : "hover:bg-zinc-800/60"
-            }`}
-            aria-current={isActive ? "page" : undefined}
-          >
-            {link.name}
-          </a>
-        );
+        if (link.path === path) {
+          return (
+            <a
+              key={index}
+              href={link.path}
+              className="font-semibold text-pink-200 bg-zinc-800/60 duration-200 rounded-md p-2 border border-zinc-600"
+            >
+              {link.name}
+            </a>
+          );
+        } else {
+          return (
+            <a key={index} href={link.path} className="hover:bg-zinc-800/60 duration-200 rounded-md p-2">
+              {link.name}
+            </a>
+          );
+        }
       })}
-    </nav>
+    </p>
   );
 }
